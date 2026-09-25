716897b8c87793d9780d90229d195aeb2cb00f35
AlomaBarros <lomahbarros@gmail.com>
2026-09-25 00:23:01 -0300

    Mais exercícios

---
+++ Enumerar_itens_da_lista.py
@@ -0,0 +1,17 @@
+# nomes = ["Ana", "Bia", "Caio"]
+# for i, nome in enumerate(nomes):
+#     print(i, nome)
+
+
+# nomes = ["Aloma", "Matheus"]
+
+# for i, nome in enumerate(nomes):
+#     print(i, nome)
+
+nomes = ["Lucia", "Henrique", "Aloma"]
+
+for posicao, nome in enumerate(nomes):
+
+    print(posicao, nome)
+
+
---
+++ Ordenar_lista_de_forma_decrescente.py
@@ -0,0 +1,4 @@
+numeros = [3, 4, 55, 7, 25, 99]
+print(f"A lista original é: {numeros}\n")
+print(sorted(numeros))
+print(sorted(numeros,reverse=True))
